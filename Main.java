6ff93451bbc638c923f43dacd5b3a70205895daf
Russel Musasa <russelmusasa@gmail.com>
2024-09-14 14:23:15 +0100

    Initial commit

---
+++ Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        byte hourOfDay = 5;
+        System.out.println(hourOfDay);
+    }
+}
