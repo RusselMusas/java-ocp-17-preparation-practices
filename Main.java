d5986c3b5c208dbd29d3972707f6264c3c78a342
Russel Musasa <russelmusasa@gmail.com>
2024-09-15 09:05:11 +0100

    java loops - simple while

--- Main.java
+++ Main.java
@@ -5,22 +5,13 @@ public class Main {
      * @param args
      */
     public static void main(String[] args) {
-        String dayofweek = "Wednesday";
-        System.out.println(dayofweek);
-        System.out.println(printDayOfWeek(dayofweek));
+        /** Loops */
+        int count = 0;
+        while (count < 10) {
+            int result = count * 10;
+            System.out.println(result);
+            count++;
+        }
     }
 
-    public static int printDayOfWeek(String dayofweek) {
-        int result = switch(dayofweek) {
-            case "Monday" -> { yield 1; }
-            case "Tuesday" -> 2;
-            case "Wednesday" -> 3;
-            case "Thursday" -> { yield 4; }
-            case "Friday" -> { yield 5; }
-            case "Saturday" -> { yield 6; }
-            case "Sunday" -> 7;
-            default -> throw new IllegalArgumentException("Unexpected value: " + dayofweek);
-        };
-        return result;
-    }
 }
