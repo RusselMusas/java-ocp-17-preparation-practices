03c60dea8ff3f44903a9587ef1426dcaa5134dda
Russel Musasa <russelmusasa@gmail.com>
2024-09-15 09:35:31 +0100

    use of simple for loop

--- Main.java
+++ Main.java
@@ -5,12 +5,9 @@ public class Main {
      * @param args
      */
     public static void main(String[] args) {
-        /** Loops - do while */
-        int count = 0;
-        do {
-            int result = count * 10;
-            System.out.println(result);
-            count++;
-        } while (count > 0 && count <= 10);
+        /** Loops - for */
+        for(int count = 0; count >= 0 && count <= 10; count++) {
+            System.out.println("Count value is: " + count);
+        }
     }
 }
