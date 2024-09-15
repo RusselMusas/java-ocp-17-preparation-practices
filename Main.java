96624255118e14598947f3b5cc4b41ed46df7d2c
Russel Musasa <russelmusasa@gmail.com>
2024-09-15 10:07:09 +0100

    For loop - reverse order

--- Main.java
+++ Main.java
@@ -5,8 +5,8 @@ public class Main {
      * @param args
      */
     public static void main(String[] args) {
-        /** Loops - for */
-        for(int count = 0; count >= 0 && count <= 10; count++) {
+        /** Loops - for loop print -> in reverse order */
+        for(int count = 10; count >= 0; count--) {
             System.out.println("Count value is: " + count);
         }
     }
