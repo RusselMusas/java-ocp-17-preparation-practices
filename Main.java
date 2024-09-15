cda79b2f55a0be0930bb0578b441892796530a60
Russel Musasa <russelmusasa@gmail.com>
2024-09-15 07:15:27 +0100

    use of switch expression - 01

--- Main.java
+++ Main.java
@@ -5,7 +5,22 @@ public class Main {
      * @param args
      */
     public static void main(String[] args) {
-        byte hourOfDay = 5;
-        System.out.println(hourOfDay);
+        byte day = 3;
+        System.out.println(day);
+        System.out.println(printDayOfWeek(day));
+    }
+
+    public static String printDayOfWeek(int day) {
+        String result = switch(day) {
+            case 1 -> "Monday";
+            case 2 -> "Tuesday";
+            case 3 -> "Wednesday";
+            case 4 -> "Thursday";
+            case 5 -> "Friday";
+            case 6 -> "Saturday";
+            case 7 -> "Sunday";
+            default -> "Invalid day";
+        };
+        return result;
     }
 }
