c2e84dfea39a3b850059300755538708f3422c9f
Russel Musasa <russelmusasa@gmail.com>
2024-09-15 08:25:12 +0100

    use of switch expression with String variable - default branch required

--- Main.java
+++ Main.java
@@ -5,27 +5,21 @@ public class Main {
      * @param args
      */
     public static void main(String[] args) {
-        byte age = 2;
-        System.out.println(age);
-        System.out.println(printDayOfWeek(age));
+        String dayofweek = "Wednesday";
+        System.out.println(dayofweek);
+        System.out.println(printDayOfWeek(dayofweek));
     }
 
-    public static String printDayOfWeek(int age) {
-        String result = switch(age) {
-            case 0, 1, 2, 3 -> {
-                if(age == 0) {
-                    yield "Baby Lavel 1";
-                } else if(age == 1) {
-                    yield "Baby Lavel 2";
-                } else if(age == 2) {
-                    yield "Baby Lavel 3";
-                } else {
-                    yield "Baby Level 3";
-                }
-            }
-            case 4, 5, 6 -> {yield "Child";}
-            case 7, 8, 9, 10, 11, 12 -> "young";
-            default -> "Invalid age";
+    public static int printDayOfWeek(String dayofweek) {
+        int result = switch(dayofweek) {
+            case "Monday" -> { yield 1; }
+            case "Tuesday" -> 2;
+            case "Wednesday" -> 3;
+            case "Thursday" -> { yield 4; }
+            case "Friday" -> { yield 5; }
+            case "Saturday" -> { yield 6; }
+            case "Sunday" -> 7;
+            default -> throw new IllegalArgumentException("Unexpected value: " + dayofweek);
         };
         return result;
     }
