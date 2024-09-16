5d505408358064e33e0ca3a559eb208df19ea357
Russel Musasa <russelmusasa@gmail.com>
2024-09-16 18:58:43 +0100

    use of Optional Labels - break the outer loop

--- Main.java
+++ Main.java
@@ -6,17 +6,27 @@ public class Main {
      * @param args
      */
     public static void main(String[] args) {
-        /** Loops - Nested Loops */
+        /** Loops - Adding Optional Labels to Loops, Switch or If-Else */
         int[][] matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
         printMatrix(matrix);
         System.out.println("=========================");
         printMatrixWithForEachLoop(matrix);
+
+        int sum = 17;
+        BAD_IDEA: if(sum > 15)
+        EVEN_WORSE_IDEA: {
+            sum += 5;
+        }
+        System.out.println("sum = " + sum);
     }
 
     public static void printMatrix(int[][] matrix) {
-        for(int i = 0; i < matrix.length; i++) {
-            for(int j = 0; j < matrix[i].length; j++)
-                System.out.print(matrix[i][j] + " ");
+        OUTER_LOOP: for(int i = 0; i < matrix.length; i++) {
+            INNER_LOOP: for(int j = 0; j < matrix[i].length; j++) {
+                            if(matrix[i][j] == 4)
+                                break OUTER_LOOP;
+                            System.out.print(matrix[i][j] + " ");
+            }
             System.out.println();
         }
     }
