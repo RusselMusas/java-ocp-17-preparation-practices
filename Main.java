897b6073e191c066bff8fdb1328471cd93bd0617
Russel Musasa <russelmusasa@gmail.com>
2024-09-16 15:20:33 +0100

    use of for-each loop

--- Main.java
+++ Main.java
@@ -5,8 +5,17 @@ public class Main {
      * @param args
      */
     public static void main(String[] args) {
-        /** Loops - for loop print -> infinite loop */
-        for(;;) 
-            System.out.println("Hello World");
+        /** Loops - for-each loop */
+        /** Can be used with: 
+         *      Built-in java array
+         *      Object whose type implements java.lang.iterable (collection) */
+        String[] names = {"John", "Jane", "Joe"};
+        Main main = new Main();
+        main.printNames(names);
+    }
+
+    public void printNames(String[] names) {
+        for (String name : names) 
+            System.out.println(name);
     }
 }
