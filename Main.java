64a9e460278f36ef684364f23f1126082a0d8cd4
Russel Musasa <russelmusasa@gmail.com>
2024-09-18 09:53:48 +0100

    java core apis - String methods length & charAt

--- Main.java
+++ Main.java
@@ -7,23 +7,16 @@ public class Main {
      * @param args
      */
     public static void main(String[] args) {
+        /** String methods */
         String name = """
                 Russel
                 Micha""";
-        System.out.println("name: \n" + name);
-        System.out.println("=========");
-        int a = 5;
-        System.out.println("6" + 5);
-        System.out.println(a + 6);
-        System.out.println(a + "6");
-        System.out.println("""
-                           Marlene
-                           """ + name);
-        System.out.println(a + 2 + 6 + "6"); // 136
-        System.out.println("6" + a + 2 + 6); // 6526
-        System.out.println("6" + (a + 2 + 6)); // 613
-        System.out.println("null" + null);
-        System.out.println("azerty" + null);
+        System.out.println("length: " + name.length());
+        System.out.println("Char At 0: " + name.charAt(0)); // R
+        System.out.println("Char At 2: " + name.charAt(2)); // s
+        System.out.println("Char At 6: " + name.charAt(6));
+        System.out.println("Char At 7: " + name.charAt(7)); // M
+        System.out.println("Char At 15: " + name.charAt(15)); // java.lang.StringIndexOutOfBoundsException
     }
 
 }
