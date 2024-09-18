57d5893357fc4ce51f711cb6d94dcfcef3be3985
Russel Musasa <russelmusasa@gmail.com>
2024-09-18 09:26:40 +0100

    java core apis - String concatenation

--- Main.java
+++ Main.java
@@ -12,6 +12,10 @@ public class Main {
                 Micha""";
         System.out.println("name: \n" + name);
         System.out.println("=========");
+        int a = 5;
+        System.out.println("6" + 5);
+        System.out.println(a + 6);
+        System.out.println(a + "6");
     }
 
 }
